32551fe4a9e409ef57a61dbb66d7af3a8f11f543
Guntaporn Pornmanee <klasun12345@gmail.com>
2022-09-04 23:33:41 +0700

    firstcommit

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,14 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if (word === undefined  || word === null)
+  {
+    return undefined;
+  } 
+  if (word.includes(startWord)){
+    return word;
+  }
+  return startWord +word;
 }
+
 module.exports = fillStartWord
